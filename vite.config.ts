aad5821fa993236522f2b78d8ca677b62afe595d
diplmaf <lizanikolaeva432@gmail.com>
2025-12-07 19:06:45 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import { resolve } from 'path';
 import svgLoader from 'vite-svg-loader';
 
 export default defineConfig({
@@ -8,4 +9,10 @@ export default defineConfig({
   build: { target: 'es2022' },
 
   plugins: [vue(), svgLoader()],
+   resolve: { 
+    alias: {
+      '@': resolve(__dirname, './src'),
+    },
+  },
+})
 });

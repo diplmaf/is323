c6b1690a1c783b17c749f440edd67a30e079efa5
diplmaf <lizanikolaeva432@gmail.com>
2025-12-07 18:58:01 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,20 @@
 <template>
-  <main>
-    <RouterView />
-  </main>
+  <div id="app">
+    <HomePage />
+  </div>
 </template>
+
+<script setup lang="ts">
+import HomePage from "@/pages/HomePage.vue";
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
